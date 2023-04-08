28623f64dd5dc2aec00543f43f9c0742adc1c623
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2023-04-08 09:23:17 -0400

    adding assignment 4.2

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,16 @@
+import { FoodModel } from "./food-model.js";
+
+export class CalorieConverter {
+    static data = [
+        new FoodModel(1007, 'Egg', 78),
+        new FoodModel(1008, 'Apple', 95),
+        new FoodModel(1009, 'Hamburger', 354),
+        new FoodModel(1010, 'Fries', 400),
+        new FoodModel(1011, 'Banana', 105),
+        new FoodModel(1012, 'Soda', 150)
+    ];
+
+    static find(name) {
+        return CalorieConverter.data.filter(food => food.name.toLowerCase() === name);
+    }
+}
---
+++ week-4/food-model.js
@@ -0,0 +1,7 @@
+export class FoodModel {
+    constructor(id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+};
